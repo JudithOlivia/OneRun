3419fca617193c0a91bbbdd546e2aaa34a191e6f
Judith Olivia <jxdith13@gmail.com>
2025-10-06 20:46:16 -0400

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+let move_speed = 3, gravity = 0.5;
+let bird = document.querySelector('.superman');
+let img = document.getElementById('superman-1');
+
+let bird_props = bird.getBoundingClientRect();
+
